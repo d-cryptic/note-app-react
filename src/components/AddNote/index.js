4e2790414a5eff0b6462303e2846663428f0b802
Barun Debnath <barundebnath91@gmail.com>
2021-11-07 22:33:21 +0530

    entered text can be accessed

--- src/components/AddNote/index.js
+++ src/components/AddNote/index.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AddNote.css";
 
-const AddNote = () => {
+const AddNote = ({ handleAddNote }) => {
+  const [noteText, setNoteText] = useState("");
+
+  // text area value
+  const handleChange = (event) => {
+    setNoteText(event.target.value);
+  };
+
+  // save button click
+  const handleSaveClick = () => {
+    handleAddNote(noteText);
+  };
+
   return (
     <div className="note new">
       <textarea
         rows="8"
         cols="10"
-        placeholder="Type to add a note..."></textarea>
+        placeholder="Type to add a note..."
+        value={noteText}
+        onChange={handleChange}></textarea>
       <div className="note-footer">
         <small>200 Remaining</small>
-        <button className="save">Save</button>
+        <button className="save" onClick={handleSaveClick}>
+          Save
+        </button>
       </div>
     </div>
   );
